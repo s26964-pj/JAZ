935d71f587a3de49784873d52f24123b22936314
dludwinsk1 <dludwinsk1@users.noreply.github.com>
2024-06-12 08:17:51 +0200

    google

--- google/src/main/java/org/example/google/GoogleApplication.java
+++ google/src/main/java/org/example/google/GoogleApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GoogleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GoogleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleApplication.class, args);
+    }
 
 }
